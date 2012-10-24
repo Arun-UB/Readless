b01b0c0fe81e74b332f300d07c0c147d7f4cf9b1
Akarsh E S <akarsh.es@gmail.com>
2012-10-24 22:59:54 +0530

    Added argparse and made strip_html a function.

--- strip_html.py
+++ strip_html.py
@@ -1,15 +1,25 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from urllib.parse import urlencode
-import re
+import re, argparse, sys
 
-html_data = BeautifulSoup(urlopen("http://www.instapaper.com/m?%s" % urlencode({'u':'http://www.theverge.com/2012/10/23/3540550/microsoft-surface-review'})).read())
-html_data = html_data.find('body')
+def strip_html(url):
+    html_data = BeautifulSoup(urlopen("http://www.instapaper.com/m?%s" % urlencode({'u':url})).read())
+    html_data = html_data.find('body')
+    
+    anchors = html_data.findAll('a')
+    for anchor in anchors:
+        	anchor.extract()
 
-anchors = html_data.findAll('a')
-for anchor in anchors:
-	anchor.extract()
+    raw = re.sub('[\n\r"\t|,]','',html_data.get_text())
 
-raw = re.sub('[\n\r"\t|“,”◆]','',html_data.get_text())
-
-print (raw)
+    print (raw)
+	#return (raw)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description = "Accepts a URL")
+    parser.add_argument("--url",dest = "url")
+    urls = parser.parse_args()
+    if urls.url == None:
+        print ("No URL Specified")
+        sys.exit()
+    strip_html(urls.url)
