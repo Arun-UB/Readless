5bf85f94118a94cc8c060f86645dee11164f55c0
ajy <ajygvnkr@gmail.com>
2012-10-27 13:36:25 +0530

    modified config variables to use environment variables

--- dev_conf.py
+++ dev_conf.py
@@ -1,8 +1,9 @@
 '''
   The db setup when running as a development server
 '''
-DATABASE = ''
+import os
+DATABASE = os.environ['MONGO_DEV_DB']
 DEBUG = True
-SECRET_KEY = ''
-USERNAME = ''
-PASSWORD = ''
+SECRET_KEY = os.environ['SECRET_KEY']
+USERNAME = os.environ['MONGO_DB_USERNAME']
+PASSWORD = os.environ['MONGO_DB_PASSWORD']
