29c9967e7de1ab94d52e101cc476d325dede0e01
ajy <ajygvnkr@gmail.com>
2012-10-29 06:33:36 +0530

    just redoing the mongo configuration variable names

--- settings.py
+++ settings.py
@@ -2,16 +2,16 @@
 import os
 class Config(object):
     SECRET_KEY = os.environ['SECRET_KEY']
-    USERNAME = os.environ['MONGO_DB_USERNAME']
-    PASSWORD = os.environ['MONGO_DB_PASSWORD']
+    MONGO_USERNAME = os.environ['MONGO_DB_USERNAME']
+    MONGO_PASSWORD = os.environ['MONGO_DB_PASSWORD']
 
 class TestConfig(Config):
     DEBUG = True
-    DATABASE = os.environ['MONGO_TEST_DB']
+    MONGO_DBNAME = os.environ['MONGO_TEST_DB']
 
 class DevConfig(Config):
     DEBUG = True
-    DATABASE = os.environ['MONGO_DEV_DB']
+    MONGO_DBNAME = os.environ['MONGO_DEV_DB']
 
 class ProdConfig(Config):
 #empty until we get a production environment
