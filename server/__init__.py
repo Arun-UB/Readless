49c3c24fb54e2a0e4a1022bd41e7d5e24190a636
ajy <ajygvnkr@gmail.com>
2012-11-15 20:15:39 +0530

    Modified settings and server to use mongoengine

--- server/__init__.py
+++ server/__init__.py
@@ -1,33 +1,10 @@
 """
   All flask server code goes here
 """
-from pymongo import Connection
-from pymongo.errors import AutoReconnect
 from flask import Flask, g
+from flask.ext.mongoengine import MongoEngine
 import settings
 import os
-
-def create_db_connection(config):
-  try:#to use the configured server
-    server=config['MONGO_SERVER']
-  except KeyError:#unless you can't
-    server='localhost'
-  mongodburi='mongodb://%(username)s:%(password)s@%(server)s/%(database)s' % \
-      {
-          "username":config['MONGO_USERNAME'], 
-          "password":config['MONGO_PASSWORD'],
-          "database":config['MONGO_DBNAME'],
-          "server":server
-          }
-
-  try:
-    connection = Connection(host = mongodburi)
-  except AutoReconnect:#might occur
-    #and i'm supposed to just let it
-    pass
-
-  db = connection[config['MONGO_DBNAME']]
-  return db
 
 #create the application
 app = Flask(__name__)
@@ -39,7 +16,7 @@ if os.environ.get('ENV')=='dev':
   app.config.from_object('settings.DevConfig')
 
 #create the db connection
-db = create_db_connection(app.config)
+db = MongoEngine(app)
 
 # load server routes
 import routes
