0580f2f34b0783ec27b4f6aecdf61869cb18420e
Akarsh E S <akarsh.es@gmail.com>
2012-11-15 20:27:48 +0530

    Made strip_html to return a list of words in the article.

--- strip_html.py
+++ strip_html.py
@@ -1,19 +1,22 @@
 from bs4 import BeautifulSoup
 import urllib
 import re, argparse, sys
+import string
 
 def strip_html(url):
     html_data = BeautifulSoup(urllib.urlopen("http://www.instapaper.com/m?%s" % urllib.urlencode({'u':url})).read())
-    html_data = html_data.find('body')
-    
-    anchors = html_data.findAll('a')
-    for anchor in anchors:
-        	anchor.extract()
+    html_data = html_data.find("body")
+    pattern = re.compile('[\W_ ]+')    
+    words = html_data.findAll(text=True)
+    word_list = []
+    for word in words[13:]:
+	for w in word.split(" "):
+	    wd = (pattern.sub('',w.lower()))
+	    if len(wd) > 1: word_list.append(wd)
 
-    raw = re.sub('[\n\r"\t|,]','',html_data.get_text())
 
-    print (raw)
-	#return (raw)
+    print (word_list)
+    return(word_list)
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description = "Accepts a URL")
     parser.add_argument("--url",dest = "url")
