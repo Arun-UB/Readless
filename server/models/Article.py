30d4350a9d6ccdb865429617de31b34a7c6719a7
ajy <ajygvnkr@gmail.com>
2012-11-16 11:53:11 +0530

    Added initial implementation for Reader embedded doc

--- server/models/Article.py
+++ server/models/Article.py
@@ -6,10 +6,11 @@ class Features(db.EmbeddedDocument):
 
 class Reader(db.EmbeddedDocument):
     #TODO:implementation
-    pass
+    user = db.ReferenceField('User', dbref = False)# the false setting here will make mongoengine use ObjectId strings here, i think
+    score = db.FloatField(min_value = 0, max_value = 1, default = 0.5)
 
 class Article(db.Document):
     source_url = db.URLField(verify_exists = True)
     feed = db.ReferenceField('Feed', dbref = True)#lazily dereferenced on access
     features = db.EmbeddedDocumentField('Features')
-    readers = db.ListField( db.EmbeddedDocumentField('Reader') )
+    readers = db.ListField(db.EmbeddedDocumentField('Reader'))
