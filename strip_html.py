e1df555761adc0009384ac965567bcbad1825964
Akarsh E S <akarsh.es@gmail.com>
2012-11-17 21:18:14 +0530

    Added code to try out classifier.

--- strip_html.py
+++ strip_html.py
@@ -2,6 +2,9 @@ from bs4 import BeautifulSoup
 import urllib
 import re, argparse, sys
 import string
+from nltk.classify import PositiveNaiveBayesClassifier
+import nltk
+
 
 def get_words_in_article(url):
     '''Arguments : URL
@@ -16,8 +19,8 @@ def get_words_in_article(url):
 	for w in word.split(" "):		#splitting on spcae for multiword strings
 	    wd = (pattern.sub('',w.lower()))	#substituing non alphanumeric characters with ''
 	    if len(wd) > 1 : word_list.append(wd)#exclude 0 & 1 character strings
-    print (word_list) #only for testing
-    return(word_list)
+    filtered_words = [w for w in word_list if not w in nltk.corpus.stopwords.words('english')]
+    return dict((word,True) for word in word_list)
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description = "Accepts a URL")
     parser.add_argument("--url",dest = "url") #Extracts url from command line, if available
@@ -25,4 +28,8 @@ if __name__ == '__main__':
     if urls.url == None:
         print ("No URL Specified")
         sys.exit()
-    strip_html(urls.url)
+    positive_examples = map(get_words_in_article, ['http://www.engadget.com/2012/11/16/htc-droid-dna-review/', 'http://www.engadget.com/2012/10/08/samsung-galaxy-note-ii-review/', 'http://www.engadget.com/2012/11/16/htc-desire-x-review/', 'http://www.engadget.com/2012/11/16/htc-desire-x-review/'])
+    misc_examples = map(get_words_in_article, ['http://www.engadget.com/2012/11/16/sharp-aquos-sh930w-reviewed-early-in-russia-with-1080p-display/', 'http://www.engadget.com/2012/11/15/nexus-4-backordered/', 'http://www.engadget.com/2012/11/16/htc-windows-phone-8x-t-mobile-review/', 'http://www.engadget.com/2012/11/16/distro-issue-66-holiday-gift-guide/', 'http://www.engadget.com/2012/10/29/apple-macbook-pro-with-retina-display-review-13-inch/', 'http://www.engadget.com/2012/11/17/skydrive-sdk-net-windows-phone-8/'])
+    classifier = PositiveNaiveBayesClassifier.train(positive_examples,misc_examples)
+
+    print classifier.classify(get_words_in_article(urls.url))
