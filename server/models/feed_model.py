1307f5d87866c89282625b6bfea4dccac6c0a740
ajy <ajygvnkr@gmail.com>
2012-11-19 21:03:14 +0530

    loading all models into models package

--- server/models/feed_model.py
+++ server/models/feed_model.py
@@ -4,7 +4,7 @@ import datetime
 class Feed(db.Document):
     name = db.StringField(required = True)
     site_url = db.URLField(verify_exists = True)
-    rss_url = db.URLField(verify_exists = True)
+    rss_url = db.URLField(verify_exists = True, unique = True, required = True)
     last_update = db.DateTimeField(default = datetime.datetime.now)#required?
     last_new_article = db.ReferenceField('Article', dbref = True)#lazily dereferenced on access
 
