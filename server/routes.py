005226560fa9db80c49396fbe6c46397e8e0c391
ajy <ajygvnkr@gmail.com>
2012-11-20 22:43:35 +0530

    added commentary to routes file

--- server/routes.py
+++ server/routes.py
@@ -29,12 +29,14 @@ def login():
 @app.route("/logout")
 @login_required
 def logout():
+    '''all logic to correctly logout a user'''
     logout_user()
     flash('logged out')
     return redirect(url_for('login'))
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    '''all logic to correctly signup a user'''
     error = None
     if request.method == 'POST':
         new_user = User(\
@@ -59,40 +61,50 @@ def signup():
 
 @app.route('/index')
 def index():
+    '''a simple index page'''
     return render_template('index.html')
 
 @app.route('/markRead/<article_id>')
 @login_required
 def MarkRead(article_id):
+    '''Logic to atomically mark an article as being read by the logged in user'''
     #TODO: test it
+    #atomically remove current user from readers of given article
     Article.objects.filter(id = article_id).update_one(pull__readers__id = current_user.id)
     return jsonify(dict(status = 'Success'))
 
 @app.route('/markUnread/<article_id>')
 @login_required
 def MarkUnread(article_id):
+    '''Logic to atomically mark an article as being unread by the logged in user'''
     #TODO: test it
     new_reader = Reader(user = current_user)
+    #atomically add current user to readers of given article
     Article.objects.filter(id = article_id).update_one(add_to_set__readers = new_reader)
     return jsonify(dict(status = 'Success'))
 
 @app.route('/subscribe/<path:rss_url>/<category>')
 @login_required
 def Subscribe(rss_url, category):
+    '''Logic to add a given rss feed to the db, if required, and subscribe the current user to this feed'''
     try:
         feed = Feed.get_or_construct(rss_url)
     except NotAFeed:
         return jsonify(dict(status = 'Error', message='The given url is not an rss feed url'))
     new_subscription = Subscription(feed_id = feed.id, category = category)
+    #atomically add new feed subscription to current user
     User.objects.filter(id = current_user.id).update_one(add_to_set__subscriptions = new_subscription)
     return jsonify(dict(status = 'Success'))
 
 @app.route('/unsubscribe/<path:rss_id>')
 @login_required
 def Unsubscribe(rss_id):
+    '''Logic to unsubscribe a user from an rss feed, and all articles from that feed'''
     try:
         feed_to_be_removed = Feed.objects.get(id = rss_id)
+        #atomically remove feed subscription from current user
         User.objects.filter(id = current_user.id).update_one(pull__subscriptions__feed_id = feed_to_be_removed.id)
+        #atomically remove articles from unsubscribed feed for current user
         Article. objects.filter(feed_id = feed_to_be_removed.id).update(pull__readers__user_id = current_user.id)
         return jsonify(dict(status = 'Success'))
     except DoesNotExist:
@@ -101,6 +113,7 @@ def Unsubscribe(rss_id):
 @app.route('/getSubscribed')
 @login_required
 def GetSubscribed():
+    '''Logic to get all feeds that a user has been subscribed to'''
     items = []
     for subscription in current_user.subscriptions:
         feed = Feed.objects.get(id = subscription.feed_id)
