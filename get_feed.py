53c86fd3f4c916ab7cd36b20910c2f4bc797bba1
Akarsh E S <akarsh.es@gmail.com>
2012-11-24 15:31:58 +0530

    added get_feed.py

--- get_feed.py
+++ get_feed.py
@@ -1,12 +1,18 @@
-import strip_html
+#import strip_html
 import feedparser
+import json
+import sys
 
 def get_feed(url):
 	feed = feedparser.parse(url)
 	if len(feed.feed) is 0:
 		print ("Error in feed URL, check feed url/network connectivity")
+		sys.exit(0)
+	ret = {'articles':[]}
+	
 	for item in feed.entries:
-		print strip_html.strip_html(item.link)
+		ret['articles'].append((item.title,item.link,item.description))
+	print json.dumps(ret['articles'][0])
 
 if __name__ == '__main__':
-	get_feed("http://www.engadget.com/rss.xml")
+	get_feed('http://feeds.feedburner.com/Phoronix')#"http://www.engadget.com/rss.xml")
