c38f6a155f6a6057109aaf92f96337bbac20cb55
Arun-UB <arun.ub29@gmail.com>
2012-11-24 20:54:08 +0530

    Minor angular additions

--- server/static/app/js/controllers.js
+++ server/static/app/js/controllers.js
@@ -2,7 +2,26 @@
 
 /* Controllers */
 
+function SubscribeCtrl($scope,$http){
+	console.log($scope.SubscribeKwd);
+	
+	  $scope.find=function () {
+	  	console.log($scope.SubscribeKwd);
+	  	 $scope.url= document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/find?v=1.0&callback=?&q=' + encodeURIComponent($scope.SubscribeKwd);
+		$http.jsonp($scope.url).success(function find(data) {
+    console.log(data);
+  });
+	//	$scope.fUrl= parse
 
+}
+		
+
+}
+function parse (data) {
+			console.log(data);
+			//return data.reponseData.entries[0].url;
+
+		}
 function MyCtrl1() {}
 MyCtrl1.$inject = [];
 
