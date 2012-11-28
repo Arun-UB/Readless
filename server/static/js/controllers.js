3c875287373ea4162b50bdfe5eb706a665cbd1bd
Arun-UB <arun.ub29@gmail.com>
2012-11-28 15:17:23 +0530

    Implemented mark as read partially

--- server/static/js/controllers.js
+++ server/static/js/controllers.js
@@ -1,16 +1,16 @@
 'use strict';
 
 /* Controllers */
-function SubscribeCtrl($scope,$http){
+function SubscribeCtrl($scope,$http,$log){
 	
 	
 	$scope.getUserInfo=function(){
 	 	$scope.url='/getUserInfo';
 	 	$http.get($scope.url).success(function(data,status){
-	 		//console.log(data);
+	 		console.log(data);
 	 		$scope.uName=data.name.trim();
 	 		$scope.disp_feed_name={};
-
+	 		$scope.uCount={};
 	 		angular.forEach(data.subscriptions,function(sub){	
 
 	 			sub.site_url=sub.site_url.replace(/http:\/\//g,"");
@@ -21,9 +21,19 @@ function SubscribeCtrl($scope,$http){
 
 	 			}
 	 		},$scope.disp_feed_name);
+
+	 		angular.forEach(data.subscriptions,function(sub){	
+
+	 			
+	 			this[sub.feed_id]=(sub.UnreadCount);
+	 				
+	 		},$scope.uCount);
+
 	 		//console.log($scope.disp_feed_name);
+	 		$scope.allFeeds=data;
 	 		$scope.feeds=data.subscriptions;
-	 		
+	 		//$log.info($scope.feeds)
+	 		$scope.hide=true;
 	 	}
 	 	);
 	 }
@@ -58,13 +68,43 @@ function SubscribeCtrl($scope,$http){
 	 		$scope.cur_feed_name=$scope.disp_feed_name[feed_id];
 	 		$scope.cur_site_url=site_url;
 	 		$scope.articles=data.articles;
-
+	 		$scope.hide=false;
 	 			//console.log($scope.cur_site_url);
+	 			console.log($scope.articles.length);
 
 
 	 		
 	 	}
 	 	);
+	}
+
+	$scope.unsubscribe=function(feed_id){
+		$log.info(feed_id);
+		$scope.url='unsubscribe/'+feed_id;
+			$http.get($scope.url).success(function(data,status){
+	 		$log.info(data);
+	 		$scope.getUserInfo();
+	 		$scope.articles={};
+	 		$scope.cur_feed_id=null;
+	 		$scope.cur_feed_name=null;
+	 		$scope.cur_site_url=null;
+	 		$scope.hide=true;
+	 		
+
+
+	 		
+	 	}
+	 	);
+
+	}
+
+	$scope.mark=function(){
+		$log.info('here')
+		$scope.articles.pop();
+		$log.info($scope.allFeeds.subscriptions);
+		$scope.uCount[$scope.cur_feed_id]=$scope.articles.length;
+		//$scope.feeds[$scope.cur_feed_id].UnreadCount=$scope.articles.length;
+
 	}
 		
 
