9184cb192199518153285bf62a34c615cfbd6a08
Akarsh E S <akarsh.es@gmail.com>
2012-11-28 13:52:07 +0530

    Added a test classifier

---
+++ test_classifier.py
@@ -0,0 +1,43 @@
+#try classifier
+import re
+from strip_html import get_words_in_article
+from nltk.classify import PositiveNaiveBayesClassifier
+from nltk import clean_html
+import urllib
+from server import app, db
+from flask.ext.mongoengine import DoesNotExist, ValidationError
+from server import User, Feed, Article, Reader
+import nltk
+import pprint
+import pickle
+
+pattern = re.compile('[\W_ ]+')
+
+def get_words_in_title(title):
+	word_list = []
+	for w in title.split(" "):
+	    wd = (pattern.sub('',w.lower()))	
+	    if len(wd) > 1 : word_list.append(wd)
+	filtered_words = [w for w in word_list if not w in nltk.corpus.stopwords.words('english')]
+	return dict((word,True) for word in filtered_words)
+
+
+def train():
+	words = {}
+	
+	for user in User.objects.all():
+		for subscription in user.subscriptions:
+			interested_titles_list = []
+			unlabled_titles_list = []
+			for article in Article.objects(interested_users = user.id, feed_id = subscription.feed_id):
+					interested_titles_list.append(article.features.title)
+					unlabled_titles_list.append(article.features.title)
+			for article in Article.objects(uninterested_users = user.id, feed_id = subscription.feed_id):
+					unlabled_titles_list.append(article.features.title)
+			words = map(get_words_in_title,interested_titles_list)
+			#pprint.pprint(words)
+			classifier = PositiveNaiveBayesClassifier.train(words,map(get_words_in_title,unlabled_titles_list)) 
+			subscription.classifier_object = pickle.dumps(classifier)
+			subscription.save()
+	print classifier.classify(get_words_in_title("this is a test title"))
+train()
