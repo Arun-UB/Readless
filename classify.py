bc847a88677e52f45880a667bf768e144ff9a92f
Akarsh E S <akarsh.es@gmail.com>
2012-11-29 14:56:49 +0530

    Completed trainer code

--- classify.py
+++ classify.py
@@ -14,30 +14,33 @@ import pickle
 pattern = re.compile('[\W_ ]+')
 
 def get_words_in_title(title):
-	word_list = []
-	for w in title.split(" "):
-	    wd = (pattern.sub('',w.lower()))	
-	    if len(wd) > 1 : word_list.append(wd)
-	filtered_words = [w for w in word_list if not w in nltk.corpus.stopwords.words('english')]
-	return dict((word,True) for word in filtered_words)
+    word_list = []
+    for w in title.split(" "):
+        wd = (pattern.sub('',w.lower()))    
+        if len(wd) > 1 : word_list.append(wd)
+    filtered_words = [w for w in word_list if not w in nltk.corpus.stopwords.words('english')]
+    return dict((word,True) for word in filtered_words)
 
 
 def train():
-	words = {}
-	
-	for user in User.objects.all():
-		for subscription in user.subscriptions:
-			interested_titles_list = []
-			unlabled_titles_list = []
-			for article in Article.objects(interested_users = user.id, feed_id = subscription.feed_id):
-					interested_titles_list.append(article.features.title)
-					unlabled_titles_list.append(article.features.title)
-			for article in Article.objects(uninterested_users = user.id, feed_id = subscription.feed_id):
-					unlabled_titles_list.append(article.features.title)
-			words = map(get_words_in_title,interested_titles_list)
-			#pprint.pprint(words)
-			classifier = PositiveNaiveBayesClassifier.train(words,map(get_words_in_title,unlabled_titles_list)) 
-			subscription.classifier_object = pickle.dumps(classifier)
-        user.save()
-	print classifier.classify(get_words_in_title("this is a test title"))
-train()
+    """Train the clasifier"""
+    words = {}
+    
+    for user in User.objects.all():
+        print 'Training for User ' + user,
+        for subscription in user.subscriptions:
+            interested_titles_list = []
+            unlabled_titles_list = []
+            for article in Article.objects(interested_users = user.id, feed_id = subscription.feed_id):
+                    interested_titles_list.append(article.features.title)
+                    unlabled_titles_list.append(article.features.title)
+            for article in Article.objects(uninterested_users = user.id, feed_id = subscription.feed_id):
+                    unlabled_titles_list.append(article.features.title)
+            words = map(get_words_in_title,interested_titles_list)
+            classifier = PositiveNaiveBayesClassifier.train(words,map(get_words_in_title,unlabled_titles_list)) 
+            subscription.classifier_object = pickle.dumps(classifier)
+        try: 
+            user.save()
+        except Exception as e:
+            print 'Failed: %s' % e
+        print 'Classifier Saved'
