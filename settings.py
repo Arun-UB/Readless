bd9a45560d40d43f30213075e6ae0d2b444719fc
ajy <ajygvnkr@gmail.com>
2012-12-04 22:37:18 +0530

    correctly create all config classes

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 # unified settings file
+from pymongo import uri_parser
 import os
 import urlparse
 class Config(object):
@@ -8,21 +9,18 @@ class Config(object):
 
 class TestConfig(Config):
     DEBUG = True
-    MONGODB_DB = os.environ['MONGO_TEST_DB']
-
+    MONGODB_DB = os.environ.get('MONGO_TEST_DB')
+    
 class DevConfig(Config):
     DEBUG = True
-    MONGODB_DB = os.environ['MONGO_DEV_DB']
+    MONGODB_DB = os.environ.get('MONGO_DEV_DB')
 
 class ProdConfig(Config):
     #empty until we get a production environment
     DEBUG = False
-    def __init__(self):
-        mongolab_uri = os.environ.get('MONGODB_URI')
-        if mongolab_uri != None:
-            url = urlparse.urlparse(mongolab_uri)
-            self.MONGODB_USERNAME = url.username
-            self.MONGODB_PASSWORD = url.password
-            self.MONGODB_HOST = url.hostname
-            self.MONGODB_PORT = url.port
-            self.MONGODB_DB = url.path[1:]
+    mongolab_uri = os.environ.get('MONGODB_URI')
+    url = uri_parser.parse_uri(mongolab_uri)
+    MONGODB_USERNAME = url['username']
+    MONGODB_PASSWORD = url['password']
+    MONGODB_HOST,MONGODB_PORT = url['nodelist'][0]
+    MONGODB_DB = url['database']
