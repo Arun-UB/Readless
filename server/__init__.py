a4c3d4ed61f51e8781a256874fb2de9d97db2f60
ajy <ajygvnkr@gmail.com>
2012-12-04 21:17:20 +0530

    added production settings

--- server/__init__.py
+++ server/__init__.py
@@ -21,6 +21,10 @@ if os.environ.get('ENV')=='dev':
   #we are running the development server
   app.config.from_object('settings.DevConfig')
 
+if os.environ.get('ENV')=='pro':
+  #we are running the development server
+  app.config.from_object('settings.ProdConfig')
+
 #create the db connection
 db = MongoEngine(app)
 
