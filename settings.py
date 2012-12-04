83bc969a71f66dab0aed3455feda5ee72cc10677
ajy <ajygvnkr@gmail.com>
2012-12-04 20:59:33 +0530

    fixed typo

--- settings.py
+++ settings.py
@@ -15,14 +15,14 @@ class DevConfig(Config):
     MONGODB_DB = os.environ['MONGO_DEV_DB']
 
 class ProdConfig(Config):
-#empty until we get a production environment
+    #empty until we get a production environment
     DEBUG = False
-    def __init__:
-        mongolab_uri = environ.get('MONGODB_URI')
-        if mongolab_uri:
-                url = urlparse.urlparse(mongolab_uri)
-               self.MONGODB_USERNAME = url.username 
-               self.MONGODB_PASSWORD = url.password 
-               self.MONGODB_HOST = url.hostname
-               self.MONGODB_PORT = url.port
-               self.MONGODB_DB = url.path[1:]
+    def __init__(self):
+        mongolab_uri = os.environ.get('MONGODB_URI')
+        if mongolab_uri != None:
+            url = urlparse.urlparse(mongolab_uri)
+            self.MONGODB_USERNAME = url.username
+            self.MONGODB_PASSWORD = url.password
+            self.MONGODB_HOST = url.hostname
+            self.MONGODB_PORT = url.port
+            self.MONGODB_DB = url.path[1:]
