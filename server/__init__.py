964433fcac3f01efec7c92cb909fd6840788cd1d
ajy <ajygvnkr@gmail.com>
2012-12-04 23:09:00 +0530

    minor fix to make sure we are using correct srever config always

--- server/__init__.py
+++ server/__init__.py
@@ -16,9 +16,8 @@ app.jinja_env.variable_start_string = '{['
 app.jinja_env.variable_end_string = ']}'
 
 #set the correct configuration
-if os.environ.get('ENV')=='dev':
-  #we are running the development server
-  app.config.from_object('settings.DevConfig')
+#we are running the development server, by default
+app.config.from_object('settings.DevConfig')
 
 if os.environ.get('ENV')=='pro':
   #we are running the development server
