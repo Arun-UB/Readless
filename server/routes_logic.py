24762ed668bd849db6b22b176910066275a4fee4
ajy <ajygvnkr@gmail.com>
2012-12-05 20:31:58 +0530

    user is subscribed to recent articles only

--- server/routes_logic.py
+++ server/routes_logic.py
@@ -128,8 +128,9 @@ def subscribe(rss_url):
     #atomically add new feed subscription to current user
     number_of_items_affected = User.objects(id = current_user.id).update_one(add_to_set__subscriptions = new_subscription)
     new_reader = Reader(user_id = current_user.id)
-    #atomically add current user to readers of all articles in subscribed feed
-    Article.objects(feed_id = feed.id).update(add_to_set__readers = new_reader)
+    #add current user to readers of all articles in subscribed feed, currently cannot be chained into one statement
+    for article in Article.objects(feed_id = feed.id).order_by("-time_stamp").limit(10):
+        article.update(add_to_set__readers = new_reader)
     if number_of_items_affected is 1:
         return jsonify(dict(status = 'Success'))
     else:
